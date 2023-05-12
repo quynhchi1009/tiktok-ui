b68be8bde1b4168992b740ecb3f9493fcfc76761
Anh Tai Duong <quynhchi1009@yandex.com>
2023-05-12 18:16:41 +0200

    Giải quyết vấn đề nhiều file index.js

---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+import config from '~/config';
+
+//Layouts
+import { HeaderOnly } from '~/layouts';
+
+//Pages
+import Home from '~/Pages/Home';
+import Following from '~/Pages/Following';
+import Profile from '~/Pages/Prpfile';
+import Upload from '~/Pages/Upload';
+import Search from '~/Pages/Search';
+
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.search, component: Search, layout: null },
+];
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
